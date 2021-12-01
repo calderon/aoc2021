20326b3dfd483b28b0abbcc850f343a7579653df
Daniel Calderón <daniel.calderon.portillo@gmail.com>
2021-12-01 20:40:30 +0100

    #1 Two different versions to parse our data, one promised, another using async, just for learning purposes

--- day01/src/report_parser.js
+++ day01/src/report_parser.js
@@ -1,14 +1,24 @@
 import { readFileSync } from 'fs';
 
 class ReportParser {
-  static get(path) {
+  static promisedGet(path) {
     return new Promise((resolve, reject) => {
       const data = readFileSync(path, 'utf8');
-
       const measures = data.split(/\r?\n/).map(measure => Number(measure));
 
       resolve(measures);
     });
+  }
+
+  static async get(path) {
+    try {
+      const data = readFileSync(path, 'utf8');
+      const measures = data.split(/\r?\n/).map(measure => Number(measure));
+      
+      return measures;
+    } catch (error) {
+      return [];
+    }
   }
 }
 
